4d40851ff972643e0b07745cf75de9342d1b661b
Dagmawit Dagne <dagmawitdagne0707@gmail.com>
2026-06-16 20:10:19 +0300

    Complete task 3 hypothesis testing

--- src/hypothesis_tests.py
+++ src/hypothesis_tests.py
@@ -1,33 +1,43 @@
 import scipy.stats as stats
+import pandas as pd
 
 
 def chi_square_test(group1, group2):
-    table = [
-        group1,
-        group2
-    ]
+    """
+    Chi-square test for categorical variables
+    """
+    table = pd.crosstab(group1, group2)
 
     chi2, p, dof, expected = stats.chi2_contingency(table)
 
-    return p
-
+    return {
+        "test": "Chi-square",
+        "statistic": chi2,
+        "p_value": p
+    }
 
 
 def t_test(group1, group2):
+    """
+    Independent t-test for numerical variables
+    """
 
-    statistic, p = stats.ttest_ind(
+    t_stat, p_value = stats.ttest_ind(
         group1,
         group2,
         equal_var=False
     )
 
-    return p
+    return {
+        "test": "Independent t-test",
+        "statistic": t_stat,
+        "p_value": p_value
+    }
 
 
-
-def decision(p_value, alpha=0.05):
+def hypothesis_decision(p_value, alpha=0.05):
 
     if p_value < alpha:
         return "Reject H0"
-
-    return "Fail to reject H0"
+    else:
+        return "Fail to Reject H0"
